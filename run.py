0df877162694e7d04f2a487f475bca51bbd126ba
Hamzakbot <shu.kurenai132010@gmail.com>
2025-03-20 06:08:20 +0530

    Update run.py

--- run.py
+++ run.py
@@ -25,7 +25,7 @@ class RunBot():
   room_id = "679366e1bc4577f6e5af791c"
   bot_token = "c85b99b6b9ef44b597dc29ca2e2d452c47a3a133aafbcbb5fba7706ddd93b8e4"
   bot_file = "my"
-  bot_class = "Bot"
+  bot_class = "BlackJackBot"
 
   def __init__(self) -> None:
     self.definitions = [
